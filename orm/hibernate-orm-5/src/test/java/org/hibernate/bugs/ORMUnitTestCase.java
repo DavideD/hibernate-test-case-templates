88a82b90cf7e06ee4e10f97cf760262ac71a60fe
Davide <davide@hibernate.org>
2022-05-19 14:10:52 +0100

    [SO] Question about assertj

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
@@ -15,12 +15,21 @@
  */
 package org.hibernate.bugs;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
+
+import org.assertj.core.api.Assertions;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using its built-in unit test framework.
@@ -36,27 +45,9 @@ public class ORMUnitTestCase extends BaseCoreFunctionalTestCase {
 	// Add your entities here.
 	@Override
 	protected Class[] getAnnotatedClasses() {
-		return new Class[] {
-//				Foo.class,
-//				Bar.class
-		};
+		return new Class[] { ReferenceType.class, AnalysisType.class };
 	}
 
-	// If you use *.hbm.xml mappings, instead of annotations, add the mappings here.
-	@Override
-	protected String[] getMappings() {
-		return new String[] {
-//				"Foo.hbm.xml",
-//				"Bar.hbm.xml"
-		};
-	}
-	// If those mappings reside somewhere other than resources/org/hibernate/test, change this.
-	@Override
-	protected String getBaseForMappings() {
-		return "org/hibernate/test/";
-	}
-
-	// Add in any settings that are specific to your test.  See resources/hibernate.properties for the defaults.
 	@Override
 	protected void configure(Configuration configuration) {
 		super.configure( configuration );
@@ -69,11 +60,96 @@ public class ORMUnitTestCase extends BaseCoreFunctionalTestCase {
 	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
+		ReferenceType referenceType1 = new ReferenceType();
+		referenceType1.setId( 55L );
+		referenceType1.setName( "Reference 1" );
+
+		ReferenceType referenceType2 = new ReferenceType();
+		referenceType2.setId( 77L );
+		referenceType2.setName( "Reference 2" );
+
+		AnalysisType analysisType = new AnalysisType();
+		analysisType.setName( "Analysis" );
+		analysisType.getReferenceTypes().add( referenceType1 );
+		analysisType.getReferenceTypes().add( referenceType2 );
+
 		// BaseCoreFunctionalTestCase automatically creates the SessionFactory and provides the Session.
-		Session s = openSession();
-		Transaction tx = s.beginTransaction();
-		// Do stuff...
-		tx.commit();
-		s.close();
+		try (Session s = openSession()) {
+			Transaction tx = s.beginTransaction();
+			s.persist( referenceType1 );
+			s.persist( referenceType2 );
+			s.persist( analysisType );
+			tx.commit();
+		}
+
+		try (Session s = openSession()) {
+			final AnalysisType result = s.find( AnalysisType.class, analysisType.getName() );
+			Assertions.assertThat( result.getReferenceTypes() ).containsExactlyInAnyOrder( referenceType2, referenceType1 );
+		}
+	}
+
+	@Entity
+	public static class AnalysisType {
+		@Id
+		private String name;
+
+		@OneToMany
+		private List<ReferenceType> referenceTypes = new ArrayList<>();
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public List<ReferenceType> getReferenceTypes() {
+			return referenceTypes;
+		}
+
+		public void setReferenceTypes(List<ReferenceType> referenceTypes) {
+			this.referenceTypes = referenceTypes;
+		}
+	}
+
+	@Entity
+	public static class ReferenceType {
+		@Id
+		private Long id;
+		private String name;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( !( o instanceof ReferenceType ) ) {
+				return false;
+			}
+			ReferenceType that = (ReferenceType) o;
+			return Objects.equals( name, that.name );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( name );
+		}
 	}
 }
