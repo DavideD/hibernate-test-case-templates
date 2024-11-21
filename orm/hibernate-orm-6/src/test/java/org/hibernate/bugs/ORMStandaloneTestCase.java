169148c0a6276c9147c67bc7a6bf88a1b5675a58
Davide D'Alto <davide@hibernate.org>
2024-11-21 14:54:28 +0100

    Test array of date
    fix

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -1,12 +1,24 @@
 package org.hibernate.bugs;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a standalone test case for Hibernate ORM.  Although this is perfectly
@@ -14,27 +26,82 @@ import org.junit.jupiter.api.Test;
  */
 class ORMStandaloneTestCase {
 
-	private SessionFactory sf;
-
-	@BeforeEach
-	void setup() {
+	protected SessionFactory constructFactory(String action) {
 		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder()
-				// Add in any settings that are specific to your test. See resources/hibernate.properties for the defaults.
 				.applySetting( "hibernate.show_sql", "true" )
 				.applySetting( "hibernate.format_sql", "true" )
-				.applySetting( "hibernate.hbm2ddl.auto", "update" );
+				.applySetting( "hibernate.hbm2ddl.auto", action )
+//				.applySetting( "hibernate.type.preferred_array_jdbc_type", "VARBINARY" )
+				;
 
 		Metadata metadata = new MetadataSources( srb.build() )
-				// Add your entities here.
-				//	.addAnnotatedClass( Foo.class )
+				.addAnnotatedClass( Foo.class )
 				.buildMetadata();
 
-		sf = metadata.buildSessionFactory();
+		return metadata.buildSessionFactory();
 	}
 
-	// Add your tests, using standard JUnit 5:
 	@Test
-	void hhh123Test() throws Exception {
+	@Order( 1 )
+	public void testLocalDateTime() {
+		LocalDateTime[] dataArray = {
+				// Unix epoch start if you're in the UK
+				LocalDateTime.of( 1970, Month.JANUARY, 1, 0, 0, 0, 0 ),
+				// pre-Y2K
+				LocalDateTime.of( 1999, Month.DECEMBER, 31, 23, 59, 59, 0 ),
+				// We survived! Why was anyone worried?
+				LocalDateTime.of( 2000, Month.JANUARY, 1, 0, 0, 0, 0 ),
+				// Silence will fall!
+				LocalDateTime.of( 2010, Month.JUNE, 26, 20, 4, 0, 0 )
+		};
+		try (SessionFactory sf = constructFactory( "create" )) {
+			Foo basic = new Foo();
+			basic.localDateTimeArray = dataArray;
+			try (Session session = sf.openSession()) {
+				session.beginTransaction();
+				session.persist( basic );
+				session.getTransaction().commit();
+			}
+			try (Session session = sf.openSession()) {
+				session.beginTransaction();
+				Foo found = session.find( Foo.class, basic.id );
+				assertThat( found.localDateTimeArray ).isEqualTo( dataArray );
+				session.getTransaction().commit();
+			}
+		}
+	}
 
+
+	@Test
+	@Order( 2 )
+	public void testDate() {
+		Date[] dataArray = {Calendar.getInstance().getTime(), Calendar.getInstance().getTime()};
+
+		try (SessionFactory sf = constructFactory( "update" )) {
+			Foo basic = new Foo();
+			basic.dateArray = dataArray;
+			try (Session session = sf.openSession()) {
+				session.beginTransaction();
+				session.persist( basic );
+				session.getTransaction().commit();
+			}
+			try (Session session = sf.openSession()) {
+				session.beginTransaction();
+				Foo found = session.find( Foo.class, basic.id );
+				for ( int i = 0; i < dataArray.length; i++ ) {
+					assertThat( found.dateArray[i].getTime() ).isEqualTo( dataArray[i].getTime() );
+				}
+				session.getTransaction().commit();
+			}
+		}
+	}
+
+	@Entity
+	public static class Foo {
+		@Id
+		@GeneratedValue
+		public Integer id;
+		public Date[] dateArray;
+		public LocalDateTime[] localDateTimeArray;
 	}
 }
