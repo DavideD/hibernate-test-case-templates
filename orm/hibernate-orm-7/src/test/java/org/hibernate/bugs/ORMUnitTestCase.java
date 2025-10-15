9ae81b6eb84a09af4fe15bf262f8e11e7d2c646c
Davide D'Alto <davide@hibernate.org>
2025-10-15 16:50:29 +0200

    RowTransformerConstructorImpl throws NullPointerException when TupleMetadata is null

--- orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
+++ orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
@@ -1,5 +1,9 @@
 package org.hibernate.bugs;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.hibernate.cfg.AvailableSettings;
 
 import org.hibernate.testing.orm.junit.DomainModel;
@@ -8,6 +12,27 @@ import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.hibernate.testing.orm.junit.Setting;
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PostRemove;
+import jakarta.persistence.PostUpdate;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.Version;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using its built-in unit test framework.
@@ -18,18 +43,7 @@ import org.junit.jupiter.api.Test;
  * What's even better?  Fork hibernate-orm itself, add your test case directly to a module's unit tests, then
  * submit it as a PR!
  */
-@DomainModel(
-		annotatedClasses = {
-				// Add your entities here.
-				// Foo.class,
-				// Bar.class
-		},
-		// If you use *.hbm.xml mappings, instead of annotations, add the mappings here.
-		xmlMappings = {
-				// "org/hibernate/test/Foo.hbm.xml",
-				// "org/hibernate/test/Bar.hbm.xml"
-		}
-)
+@DomainModel(annotatedClasses = { ORMUnitTestCase.Element.class, ORMUnitTestCase.Node.class })
 @ServiceRegistry(
 		// Add in any settings that are specific to your test.  See resources/hibernate.properties for the defaults.
 		settings = {
@@ -48,8 +62,129 @@ class ORMUnitTestCase {
 	// Add your tests, using standard JUnit 5.
 	@Test
 	void hhh123Test(SessionFactoryScope scope) throws Exception {
+		Node basik = new Node( "Child" );
+		basik.parent = new Node( "Parent" );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
+		basik.elements.add( new Element( basik ) );
+
+		scope.inTransaction( session -> session.persist( basik ) );
 		scope.inTransaction( session -> {
-			// Do stuff...
+			List<Node> resultList = session
+					.createSelectionQuery( "select distinct n from Node n left join fetch n.elements", Node.class )
+					.getResultList();
+			assertThat( resultList ).hasSize( 2 );
 		} );
+		scope.inTransaction( session -> {
+			List<Node> resultList = session
+					.createSelectionQuery( "select distinct n, e from Node n join n.elements e", Node.class )
+					.getResultList();
+			assertThat( resultList ).hasSize( 3 );
+		} );
+		scope.inTransaction( session -> {
+			List<Node> resultList = session
+					.createSelectionQuery( "select distinct n.id, e.id from Node n join n.elements e", Node.class )
+					.getResultList();
+			assertThat( resultList ).hasSize( 3 );
+		} );
+		scope.inTransaction( session -> {
+			List<Node> resultList = session
+					.createSelectionQuery( "select max(e.id), min(e.id), sum(e.id) from Node n join n.elements e group by n.id order by n.id", Node.class )
+					.getResultList();
+			assertThat( resultList ).hasSize( 1 );
+		} );
+	}
+
+
+	@Entity(name = "Element")
+	@Table(name = "Element")
+	public static class Element {
+		@Id
+		@GeneratedValue
+		Integer id;
+
+		@ManyToOne
+		Node node;
+
+		public Element(Node node) {
+			this.node = node;
+		}
+
+		public Element() {
+		}
+	}
+
+	@Entity(name = "Node")
+	@Table(name = "Node")
+	public static class Node {
+
+		@Id
+		@GeneratedValue
+		Integer id;
+		@Version
+		Integer version;
+		String string;
+
+		@ManyToOne(fetch = FetchType.LAZY,
+				cascade = {
+						CascadeType.PERSIST,
+						CascadeType.REFRESH,
+						CascadeType.MERGE,
+						CascadeType.REMOVE
+				})
+		Node parent;
+
+		@OneToMany(fetch = FetchType.EAGER,
+				cascade = {
+						CascadeType.PERSIST,
+						CascadeType.REMOVE
+				},
+				mappedBy = "node")
+		List<Element> elements = new ArrayList<>();
+
+		public Node(String string) {
+			this.string = string;
+		}
+
+		public Node() {
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getString() {
+			return string;
+		}
+
+		public void setString(String string) {
+			this.string = string;
+		}
+
+		@Override
+		public String toString() {
+			return id + ": " + string;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+			Node node = (Node) o;
+			return Objects.equals( string, node.string );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( string );
+		}
 	}
 }
